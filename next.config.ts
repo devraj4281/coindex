87675b5fd53d3fa4516c23a46bd38168d0b37fbc
devraj42817 <rajdev20774@gmail.com>
2026-02-27 22:09:30 +0530

    chore: update project config and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "assets.coingecko.com",
+            }, {
+                protocol: "https",
+                hostname: "coin-images.coingecko.com",
+            },
+        ],
+        formats: ['image/webp', 'image/avif'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        minimumCacheTTL: 60,
+    },
+    compress: true,
 };
 
 export default nextConfig;
